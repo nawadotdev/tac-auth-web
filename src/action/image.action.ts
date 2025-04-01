cdac3a45f93cac4ca5d8e5be40fa9b58e1e52901
devnawa <148815756+devnawa@users.noreply.github.com>
2025-04-01 15:05:54 +0300

    fix: base image path for server-side

--- src/action/image.action.ts
+++ src/action/image.action.ts
@@ -6,7 +6,7 @@ export const generateImage = async (username: string) => {
   const canvas = createCanvas(1920, 1080);
   const ctx = canvas.getContext("2d");
 
-  const image = await loadImage("base.jpg");
+  const image = await loadImage(`${process.cwd()}/public/base.jpg`);
   ctx.drawImage(image, 0, 0, 1920, 1080);
 
   ctx.font = "bold 50px DM Sans";
