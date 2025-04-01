df021af1989a7463033b0aafe3d37bb7ad7fc40f
devnawa <148815756+devnawa@users.noreply.github.com>
2025-04-02 00:01:45 +0300

    refactor: GET

--- src/app/api/twitter-card/route.ts
+++ src/app/api/twitter-card/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function GET(req: NextApiRequest, res: NextApiResponse) {
     const { username } = req.query;
 
     const redirectTo = "https://tac.build"
