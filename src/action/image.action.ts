0b45efdf3c68bfe31de51ed65c92ff54bb41658b
devnawa <148815756+devnawa@users.noreply.github.com>
2025-04-01 23:34:14 +0300

    update image path

--- src/action/image.action.ts
+++ src/action/image.action.ts
@@ -1,12 +1,13 @@
 "use server";
-
+import path from "path";
 import { createCanvas, loadImage } from "canvas";
 
 export const generateImage = async (username: string) => {
   const canvas = createCanvas(1920, 1080);
   const ctx = canvas.getContext("2d");
 
-  const image = await loadImage(`${process.cwd()}/public/base.jpg`);
+  const imagePath = path.resolve(process.cwd(), "public", "base.jpg");
+  const image = await loadImage(imagePath);
   ctx.drawImage(image, 0, 0, 1920, 1080);
 
   ctx.font = "bold 50px DM Sans";
